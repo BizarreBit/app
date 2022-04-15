ee268c7426ab8a879b90847a8495a28257cc6e36
BizarreBit <prawarit888@hotmail.com>
2022-04-15 23:59:51 +0700

    Lab4-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <label htmlFor="province">Province : </label>
+        <select id="province">
+          <option value="" disabled selected hidden>
+            Select
+          </option>
+          <option value="bangkok">Bangkok</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="district">District : </label>
+        <select id="district">
+          <option value="district" disabled selected hidden>
+            Select
+          </option>
+          <option value="bangchak">Bangchak</option>
+        </select>
+      </div>
     </div>
   );
 }
---
+++ src/readme.md
@@ -0,0 +1,8 @@
+Online CodeCamp 11
+
+Intro to React LAB 4 Create React App
+2. 
+- จงเขียนโค้ดเพื่อสร้าง Web ให้ได้ข้อมูลดังรูป โดยยังไม่ต้องสนใจ CSS
+- ข้อมูลใน tag <select> ให้แสดงข้อมูลของจังหวัดและอำเภอ โดยยังไม่ต้องสนใจว่าถ้าเลือกจังหวัดแล้วอำเภอจะแสดงเฉพาะอำเภอในจังหวัดนั้น
+Province : [Select v]
+District : [Select v]
