a800f57c62962c5f9e348c01de92947caf4572a5
BizarreBit <prawarit888@hotmail.com>
2022-04-16 12:49:58 +0700

    handling-event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  // const handleClick = event => console.log(event);
+  const handleClick = (event, id) => console.log(id);
+  const handleClick2 = id => event => console.log(id);
+
+  function handleSubmitForm(event) {
+    event.preventDefault();
+    console.log('Submitted')
+  }
+
+  const handleChangeEmail = function (event) {
+    console.log(event.target.value)
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button
+        onClick={function () {
+          console.log("Clicked");
+        }}
+      >
+        Click Me
+      </button> */}
+      {/* <button onClick={handleClick}>Click Me</button> */}
+      <button onClick={(event) => handleClick(event, 2)}>Click Me</button>
+      <button onClick={handleClick2(3)}>Click Me</button>
+
+      <form onSubmit={handleSubmitForm}>
+        <input name="email" onChange={handleChangeEmail} />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
