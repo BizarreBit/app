d7c112931a3abedda6891f28c145acf1dad02e4f
BizarreBit <prawarit888@hotmail.com>
2022-04-16 15:20:24 +0700

    Lab6_1-4 Before checking with The solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const handleInput = (event) => console.log(event.target.value);
+
+  const handleCheckBox = (event) => {
+    console.log(event.target.name);
+    console.log(event.target.labels[0].innerHTML);
+    console.log(event.target.checked);
+  };
+
+  const handleLink = () => {
+    if (window.confirm("Leave for https://google.com?"))
+      window.location.href = "https://google.com";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        <input onChange={handleInput} />
+      </div>
+
+      <div>
+        <select onChange={handleInput}>
+          <option>Thailand</option>
+          <option>Japan</option>
+          <option>China</option>
+          <option>Malaysia</option>
+          <option>Laos</option>
+        </select>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name="samsung"
+          id="samsung"
+          onChange={handleCheckBox}
+        />
+        <label htmlFor="samsung">Samsung</label>
+        <input
+          type="checkbox"
+          name="xiaomi"
+          id="xiaomi"
+          onChange={handleCheckBox}
+        />
+        <label htmlFor="xiaomi">Xiaomi</label>
+        <input
+          type="checkbox"
+          name="nokia"
+          id="nokia"
+          onChange={handleCheckBox}
+        />
+        <label htmlFor="nokia">Nokia</label>
+      </div>
+
+      <div>
+        <a href="#" onClick={handleLink}>
+          Google
         </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+Online CodeCamp 11
+
+Intro to React LAB 5 Handling Events
+1. จงสร้าง tag <input>
+เมื่อผู้ใช้พิมพ์ค่าลงใน <input> ให้ console.log(ค่าที่ผู้ใช้พิมพ์)
