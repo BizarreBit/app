cfcc10dcd426ce5b72cccffedf91e0dc23193f4d
BizarreBit <prawarit888@hotmail.com>
2022-04-23 19:07:44 +0700

    Lab6_2-4 After checking with the solution

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
   const handleCheckBox = (event) => {
     console.log(event.target.name);
     console.log(event.target.labels[0].innerHTML);
-    console.log(event.target.checked);
+    console.log(event.target.value);
+    console.log(event.target.checked ? 'Ticked' : 'Unticked');
   };
 
   const handleLink = () => {
     if (window.confirm("Leave for https://google.com?"))
       window.location.href = "https://google.com";
   };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const redirect = window.confirm('Leave for https://google.com?');
+    if (redirect) {
+      window.location.assign("https://google.com");
+    }
+  }
 
   return (
     <div className="App">
@@ -33,22 +42,25 @@ function App() {
       <div>
         <input
           type="checkbox"
-          name="samsung"
+          name="phoneBrand"
           id="samsung"
+          value="Samsung"
           onChange={handleCheckBox}
         />
         <label htmlFor="samsung">Samsung</label>
         <input
           type="checkbox"
-          name="xiaomi"
+          name="phoneBrand"
           id="xiaomi"
+          value="XiaoMi"
           onChange={handleCheckBox}
         />
         <label htmlFor="xiaomi">Xiaomi</label>
         <input
           type="checkbox"
-          name="nokia"
+          name="phoneBrand"
           id="nokia"
+          value="Nokia"
           onChange={handleCheckBox}
         />
         <label htmlFor="nokia">Nokia</label>
@@ -56,6 +68,10 @@ function App() {
 
       <div>
         <a href="#" onClick={handleLink}>
+          Google
+        </a>
+        <br/>
+        <a href="https://google.com" onClick={handleClick}>
           Google
         </a>
       </div>
