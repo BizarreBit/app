196bde5d33cf628ec1e6c46e7393aa7902c4bbba
BizarreBit <prawarit888@hotmail.com>
2022-04-24 16:53:57 +0700

    With Video

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import './App.css';
-import { useState } from 'react';
+import { useState } from "react";
+import "./App.css";
 
 function Count() {
   const [count, setCount] = useState(0);
-  return <div>
-    <div>{count}</div>
-    <button onClick={ () => setCount(count + 1)}>Click</button>
-  </div>;
-
+  const handleClickAdd = () => {
+    setCount(5); //Not current state : Asynchonous
+    // setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
+  }
+  return (
+    <div>
+      <div>{count}</div>
+      {/* <button onClick={() => setCount(count + 1)}>Click</button> */}
+      <button onClick={handleClickAdd}>+</button>
+    </div>
+  );
 }
 
 function App() {
+  let [country, setCountry] = useState("Thailand"); // Array [state, Update Function]
+  let [population, setPopulation] = useState(1000);
+
+  const handleChange = (event) => {
+    let value = event.target.value;
+    setCountry(value);
+    // country = value
+  };
+
   return (
     <div className="App">
       <Count />
+      <hr />
+      <input type="text" onChange={handleChange} />
+      <h1>{country}</h1>
+      <input
+        type="text"
+        onChange={(event) => setPopulation(event.target.value)}
+      />
+      <h1>{population}</h1>
     </div>
   );
 }
