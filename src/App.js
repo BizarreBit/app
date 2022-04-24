7332b4f524be21c4bbc6a57f70b7936e225f3b27
BizarreBit <prawarit888@hotmail.com>
2022-04-24 20:58:06 +0700

    list and keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import "./App.css";
+
+let id = 3;
 
 function App() {
+  const arr = [1, 2, 3, 4];
+  const jsxArr = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>];
+  const result = arr.map((el) => <li>{el}</li>);
+
+  const [obj, setObj] = useState([
+    { id: 1, title: "A" },
+    { id: 2, title: "B" },
+  ]);
+
+  const handleClick = () => {
+    const input = window.prompt("Enter Something");
+    const newState = [{ id: id++, title: input }, ...obj];
+    setObj(newState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Add</button>
+      <ul>
+        {/* <li>{arr[0]}</li>
+        <li>{arr[1]}</li>
+        <li>{arr[2]}</li>
+        <li>{arr[3]}</li> */}
+        {/* {jsxArr} */}
+        {/* {result} */}
+        {obj.map((el) => (
+          <li key={el.id}>{el.title}</li>
+        ))}
+        {/* {arr.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))} */}
+      </ul>
     </div>
   );
 }
